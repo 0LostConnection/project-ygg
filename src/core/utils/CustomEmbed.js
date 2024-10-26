f8b0b6d653e79641db69c3f5579d3f8882c1f193
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-10-26 19:30:37 -0300

    Update: CustomEmbed.js JSdocs

--- src/core/utils/CustomEmbed.js
+++ src/core/utils/CustomEmbed.js
@@ -1,4 +1,6 @@
 import { EmbedBuilder } from "@discordjs/builders";
+import { RestOrArray } from "discord.js"
+import { APIEmbedField } from "discord-api-types/v10"
 
 /**
  * Classe para criar embeds de erro personalizados para mensagens no Discord.
@@ -21,39 +23,65 @@ export class ErrorEmbed {
                 name: "Mensagem de Erro:",
                 value: error
             })
-            embed.setColor(15871037)
+            embed.setColor(15871037) // Altera a cor se um erro for fornecido
         }
-        return embed
+        return embed;
     }
 }
 
+/**
+ * Classe para criar embeds de sucesso personalizados para mensagens no Discord.
+ * Utiliza o EmbedBuilder para configurar uma mensagem de sucesso com cor e descrição.
+ */
 export class SuccessEmbed {
+    /**
+     * Construtor para a classe SuccessEmbed.
+     * @param {string} message - A mensagem de sucesso a ser exibida.
+     */
     constructor(message) {
         let embed = new EmbedBuilder()
             .setColor(6218828) // Define a cor do embed
             .setDescription(message) // Define a descrição principal do embed
 
-        return embed
+        return embed;
     }
 }
 
-export class QuestionEmebed {
+/**
+ * Classe para criar embeds de pergunta personalizados para mensagens no Discord.
+ * Utiliza o EmbedBuilder para configurar uma mensagem de pergunta com cor e descrição.
+ */
+export class QuestionEmbed {
+    /**
+     * Construtor para a classe QuestionEmbed.
+     * @param {string} message - A mensagem da pergunta a ser exibida.
+     */
     constructor(message) {
         let embed = new EmbedBuilder()
             .setColor(11489267) // Define a cor do embed
             .setDescription(message) // Define a descrição principal do embed
 
-        return embed
+        return embed;
     }
 }
 
+/**
+ * Classe para criar embeds de estoque de produtos personalizados para mensagens no Discord.
+ * Utiliza o EmbedBuilder para configurar uma mensagem de estoque com cor, descrição e campos personalizados.
+ */
 export class ProductStockEmbed {
+    /**
+     * Construtor para a classe ProductStockEmbed.
+     * @param {string} message - A mensagem descritiva do estoque a ser exibida.
+     * @param {RestOrArray<APIEmbedField>} fields - Um array de objetos representando os campos a serem adicionados ao embed. 
+     *                                  Cada objeto deve ter `name` e `value` como propriedades.
+     */
     constructor(message, fields) {
         let embed = new EmbedBuilder()
             .setColor(14581094) // Define a cor do embed
             .setDescription(message) // Define a descrição principal do embed
-            .setFields(fields)
+            .setFields(fields) // Adiciona os campos ao embed
 
-        return embed
+        return embed;
     }
 }
