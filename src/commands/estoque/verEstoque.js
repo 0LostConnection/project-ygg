457126a723c3d30a5885f0121ada78efe1ecc540
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-10-26 19:30:49 -0300

    Update verEstoque.js

--- src/commands/estoque/verEstoque.js
+++ src/commands/estoque/verEstoque.js
@@ -2,6 +2,7 @@ import CommandStructure from "../../core/structures/CommandStructure"
 import { CommandInteraction } from "discord.js"
 import EstoqueDB from "../../core/database/EstoqueDB"
 import CustomSelectMenu from "../../core/utils/customSelectMenu"
+import { ErrorEmbed, ProductStockEmbed, QuestionEmebed, SuccessEmbed } from "../../core/utils/CustomEmbed"
 
 export default class extends CommandStructure {
     constructor(interaction) {
@@ -16,22 +17,107 @@ export default class extends CommandStructure {
     * @param {CommandInteraction} interaction
     **/
     run = async (interaction) => {
+        // Deferindo a resposta para indicar que o bot está processando a solicitação
         await interaction.deferReply()
 
-        const db = new EstoqueDB(process.env.MONGODB_URI)
-        await db.connect()
+        // Instancia o banco de dados e conecta
+        const estoqueDB = new EstoqueDB()
+        await estoqueDB.connect()
 
-        const categorias = await db.listarCategorias()
-            
+        // Obtém as categorias
+        const categorias = await estoqueDB.listarCategorias()
+
+        // Valida se a operação ocorreu com sucesso
         if (!categorias.success) {
-            return interaction.editReply(`${categorias.message}`)
+            return await interaction.editReply({
+                embeds: [
+                    new ErrorEmbed(categorias.message,
+                        categorias.error ? `\`\`\`${categorias.error}\`\`\`` : null)
+                ]
+            })
         }
 
-        const selectMenu = new CustomSelectMenu("estoque:categorias", "Escolha uma categoria.", categorias.data?.map(obj => ({ label: obj.nome, value: obj.id.toString() })))
-        
-        interaction.editReply({ content: "Escolha uma categoria:", components: [selectMenu] })
+        /* 
+        Inicio da seleção das categorias.
+        Salva a interação em uma variável
+        */
+        const resposta = await interaction.editReply({
+            embeds: [
+                new QuestionEmebed("Escolha uma das categorias na lista abaixo:")
+            ],
+            components: [
+                new CustomSelectMenu(
+                    "estoque:selecionar:categorias",
+                    "Escolha uma categoria",
+                    categorias.data.map(obj => ({
+                        label: obj.nome,
+                        value: obj.id.toString()
+                    }))
+                )
+            ]
+        })
 
-        await db.disconnect()
+        // Filtro para ser utilizado para detectar a interação do StringSelectMenu
+        const filtroObserver = i => i.user.id === interaction.user.id
 
+        try {
+            // Cria uma nova interação para poder ser utilizada no observer
+            const confirmacao = await resposta.awaitMessageComponent({
+                filter: filtroObserver,
+                time: 60_000
+            })
+
+            // Verifica se a interação é um StringSelectMenu
+            if (!confirmacao.isStringSelectMenu()) return
+
+            /* 
+            1. Pega o id da categoria na seleção
+            2. Obtém o nome da categoria
+            3. Obtém a lista de itens da categoria 
+            */
+            const idCategoria = confirmacao.values[0]
+            const nomeCategoria = (await estoqueDB.obterNomeCategoria(idCategoria)).nomeCategoria
+            const itens = await estoqueDB.listarItensPorCategoria(idCategoria)
+
+            // Desconecta do banco de dados
+            estoqueDB.disconnect()
+
+            // Valida se a obtenção ocorreu com sucesso
+            if (!itens.success) {
+                return await confirmacao.update({
+                    embeds: [
+                        new ErrorEmbed(
+                            itens.message,
+                            itens.error ? `\`\`\`${itens.error}\`\`\`` : null
+                        )
+                    ]
+                })
+            }
+
+            // Transforma os dados dos itens obtidos no formato que será utilizado nos Fields do Embed
+            let estoqueArray = []
+            itens.data.forEach(item => {
+                estoqueArray.push({ name: item.nome, value: String(item.quantidade) })
+            })
+
+            // Atualiza a mensagem da interação com a lista dos itens
+            await confirmacao.update({
+                embeds: [
+                    new ProductStockEmbed(
+                        `## ${nomeCategoria}`,
+                        estoqueArray
+                    )
+                ],
+                components: []
+            })
+        } catch (error) {
+            // Caso não haja confirmação no SelectMenu, a mensagem é atualizada
+            await interaction.editReply({
+                embeds: [
+                    new ErrorEmbed("Nenhuma confirmação recebida após 1 minuto!")
+                ],
+                components: []
+            })
+        }
     }
 }
